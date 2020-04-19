57a052fcaf5cd2da5515c9fea54433a507a330c5
Steven Anderson <steve@whilefalse.net>
2020-04-19 19:57:48 +0100

    Use buildpacks instead of Docker

--- config/prod.secret.exs
+++ config/prod.secret.exs
@@ -23,7 +23,7 @@ config :mazes, MazesWeb.Endpoint,
 # If you are doing OTP releases, you need to instruct Phoenix
 # to start each relevant endpoint:
 #
-config :mazes, MazesWeb.Endpoint, server: true
+# .config :mazes, MazesWeb.Endpoint, server: true
 #
 # Then you can assemble a release by calling `mix release`.
 # See `mix help release` for more information.

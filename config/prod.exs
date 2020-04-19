7a9c55a2fa3f1fd0a4656b4d96b5d86d862fe6a9
Steven Anderson <steve@whilefalse.net>
2020-04-19 20:20:35 +0100

    Change url

--- config/prod.exs
+++ config/prod.exs
@@ -11,7 +11,7 @@ use Mix.Config
 # before starting your production server.
 config :mazes, MazesWeb.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [scheme: "https", host: "stark-waters-15176.herokuapp.com", port: 443],
+  url: [scheme: "https", host: "infinite-mazes.herokuapp.com", port: 443],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 

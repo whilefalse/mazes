c29fa3c0cd63fa332078200d56ea659753e2ebbc
Steven Anderson <steve@whilefalse.net>
2022-05-19 23:13:04 +0100

    feat: Run tests on github

--- test/mazes_web/live/page_live_test.exs
+++ test/mazes_web/live/page_live_test.exs
@@ -3,9 +3,14 @@ defmodule MazesWeb.PageLiveTest do
 
   import Phoenix.LiveViewTest
 
+  setup do
+    Mox.stub_with(Mazes.MockRandom, Mazes.Random)
+    :ok
+  end
+
   test "disconnected and connected render", %{conn: conn} do
     {:ok, page_live, disconnected_html} = live(conn, "/")
-    assert disconnected_html =~ "Welcome to Phoenix!"
-    assert render(page_live) =~ "Welcome to Phoenix!"
+    assert disconnected_html =~ "class=\"maze\""
+    assert render(page_live) =~ "class=\"maze\""
   end
 end
